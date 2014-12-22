9aa87c154b13429e376055750e8c2956e7a078a2
Nikita Karetnikov <nikita@karetnikov.org>
2014-12-22 08:05:09 +0400

    Add problem 2.

--- src/Lists.v
+++ src/Lists.v
@@ -52,3 +52,55 @@ Proof.
       inversion IHyt.
       apply IHyt.
 Qed.
+
+(** * Problem 2
+
+    Find the last but one element of a list.
+    (Note that the Lisp transcription of this problem is incorrect.)
+
+    Example in Haskell:
+<<
+Prelude> myButLast [1,2,3,4]
+3
+Prelude> myButLast ['a'..'z']
+'y'
+>>
+*)
+
+Fixpoint myButLast {X:Type} (xs:list X) : option X :=
+  match xs with
+    | [] | [_] => None
+    | [x;_]    => Some x
+    | _::xt    => myButLast xt
+  end.
+
+Example myButLast1:
+  myButLast [1;2;3;4] = Some 3.
+Proof. reflexivity. Qed.
+Open Scope char_scope.
+Example myButLast2:
+  myButLast ["a";"b";"c";"d"] = Some "c".
+Proof. reflexivity. Qed.
+Close Scope char_scope.
+
+Theorem myButLast_nil : forall X:Type,
+  @myButLast X [] = None.
+Proof. reflexivity. Qed.
+
+Theorem myButLast_one : forall (X:Type) (x:X),
+  myButLast [x] = None.
+Proof. reflexivity. Qed.
+
+Theorem myButLast_app : forall (X:Type) (xs:list X) (x:X) (y:X),
+  myButLast (app xs [x;y]) = Some x.
+Proof.
+  intros.
+  induction xs as [|z zt].
+    reflexivity.
+    simpl. destruct (zt ++ [x;y]).
+      inversion IHzt.
+      rewrite IHzt.
+      destruct l as [|n nt].
+        inversion IHzt.
+        reflexivity.
+Qed.
